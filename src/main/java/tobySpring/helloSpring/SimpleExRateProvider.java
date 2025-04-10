f18188ee39ad3d8d1435141f278b63413dbf47ca
Yu-Hyeon-Su <ydasfg@gmail.com>
2025-04-10 18:08:13 +0900

    feat: 환율정보 캐시 기능 도입을 데코레이터 패턴을 적용해서 구현

--- src/main/java/tobySpring/helloSpring/SimpleExRateProvider.java
+++ src/main/java/tobySpring/helloSpring/SimpleExRateProvider.java
@@ -1,11 +1,9 @@
 package tobySpring.helloSpring;
 
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 
-@Component
 public class SimpleExRateProvider implements ExRateProvider{
     @Override
     public BigDecimal getExRate(String currency) throws IOException {
