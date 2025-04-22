5b446cf0d50f62869e4dfa689025c28edb2d2eef
Yu-Hyeon-Su <ydasfg@gmail.com>
2025-04-22 16:45:37 +0900

    feat: Sort 테스트 작성

--- src/main/java/tobySpring/helloSpring/Client.java
+++ src/main/java/tobySpring/helloSpring/Client.java
@@ -14,12 +14,7 @@ public class Client {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
-        Payment payment1 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println("Payment1: " + payment1);
-        System.out.println("-----------------------------------\n");
-
-        TimeUnit.SECONDS.sleep(3);
-        Payment payment2 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println("Payment2: " + payment2);
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println("Payment: " + payment);
     }
 }
