427a252e1ae62c52c9694b84a01da2edb4fe70de
Yu-Hyeon-Su <ydasfg@gmail.com>
2025-06-05 13:24:34 +0900

    feat: OrderRepository에 Order관련 데이터 접근 기능 분리

--- src/main/java/tobySpring/helloSpring/payment/DataClient.java
+++ src/main/java/tobySpring/helloSpring/payment/DataClient.java
@@ -5,24 +5,19 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import tobySpring.helloSpring.data.OrderRepository;
 import tobySpring.helloSpring.order.Order;
 
 import java.math.BigDecimal;
 
 public class DataClient {
     public static void main(String[] args) {
-            BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
-            EntityManagerFactory emf = beanFactory.getBean(EntityManagerFactory.class);
-
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
+        OrderRepository orderRepository = beanFactory.getBean(OrderRepository.class);
 
         Order order = new Order("100", BigDecimal.TEN);
-        em.persist(order);
+        orderRepository.save(order);
 
         System.out.println(order);
-
-        em.getTransaction().commit();
-        em.close();
     }
 }
