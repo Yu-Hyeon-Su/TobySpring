062694f1af2a077ff23014748ffc7b47010a9141
Yu-Hyeon-Su <ydasfg@gmail.com>
2025-06-19 19:44:12 +0900

    refactor: @Transactional으로 트랜잭션 기술 변경

--- src/main/java/tobySpring/helloSpring/order/OrderConfig.java
+++ src/main/java/tobySpring/helloSpring/order/OrderConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tobySpring.helloSpring.data.JdbcOrderRepository;
 import tobySpring.helloSpring.payment.DataConfig;
 
@@ -11,16 +12,14 @@ import javax.sql.DataSource;
 
 @Configuration
 @Import(DataConfig.class)
+@EnableTransactionManagement
 public class OrderConfig {
     @Bean
     public OrderService orderService(
             PlatformTransactionManager transactionManager,
             OrderRepository orderRepository
     ) {
-        return new OrderServiceTxProxy(
-                new OrderServiceImpl(orderRepository),
-                transactionManager
-        );
+        return new OrderServiceImpl(orderRepository);
     }
 
     @Bean
